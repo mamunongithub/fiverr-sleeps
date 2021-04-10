66f9bea1e3d1fbfe89ce04b853d0b7902d91ed14
Mamun <mamunkhan.no1@gmail.com>
2021-04-10 23:10:07 +0600

    fix: favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -37,11 +37,6 @@ export default function Layout({
           sizes="16x16"
         />
 
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        />
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
